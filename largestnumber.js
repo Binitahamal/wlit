b53942a0df94e9783dcc5cd828cd813780896aa8
binita hamal <binita.hamal7@gmail.com>
2022-03-05 19:55:16 -0600

    this is my first commit

---
+++ largestnumber.js
@@ -0,0 +1,13 @@
+let x=10;
+let y=10;
+let z=15;
+if(x>y && x>z){
+    console.log("x is greater");
+}
+else if(y>x && y>z){
+    console.log("y is greater");
+}
+else{
+    console.log("z is greater");
+}
+
